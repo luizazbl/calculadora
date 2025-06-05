ba6d4759bb334f95cef5061f385084d9ec783b3f
luiza <luizaizabeldeandrade@gmail.com>
2025-06-05 10:56:05 -0300

    Create cauculadora.java

---
+++ cauculadora.java
@@ -0,0 +1,8 @@
+public abstract class Calculadora {
+    public abstract double somar(double numeroUm, double numeroDois);
+    public abstract double subtrair(double numeroUm, double numeroDois);
+    public abstract double multiplicar(double numeroUm, double numeroDois);
+    public abstract double dividir(double numeroUm, double numeroDois);
+    public abstract double potencia(double numeroUm, double numeroDois);
+    public abstract double raizQuadrada(double numeroUm);
+}
